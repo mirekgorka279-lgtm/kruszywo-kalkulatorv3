a42acae8dc761956d1198a4a9baf83968f844f27
mirekgorka279-lgtm <mirekgorka279@gmail.com>
2026-07-06 12:45:43 +0200

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    VERSION: "4.0.0",
+
+    API_URL: "https://script.google.com/macros/s/AKfycbwBA5NUDqnIvvIGDEpdba_NYBDXWgZ2AfFNZGGKF3sRBh-sMODaqyc9yGSpGl_L_-wA/exec",
+
+    CACHE_KEY: "kruszywo_price_cache",
+
+    LAST_SYNC_KEY: "kruszywo_last_sync",
+
+    DEBUG: true
+};
